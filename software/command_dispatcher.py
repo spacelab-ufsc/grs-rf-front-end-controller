6dd9df4d3df0e6f8d68445cdf953354ec840f7d4
arthurmarr <arthurmarraui@gmail.com>
2026-05-17 01:34:12 -0300

    Communication between front-end controller and server is now done using ZMQ library

--- software/command_dispatcher.py
+++ software/command_dispatcher.py
@@ -1,25 +1,18 @@
-#{
-#  "status": {
-#    "vhf": "rx",         // Options: "rx", "tx", "off"
-#    "uhf_400": "tx",     // Options: "rx", "tx", "off"
-#    "uhf_468": "rx",     // Options: "rx", "off"
-#    "s_band": "off"      // Options: "rx", "off"
-#  },
-#  "request_telemetry": true // Options: true, false
-#}
-
-import requests
+import zmq
 import json
 
-# Get raspberryPi hostname
-pi_hostname = 1
-PI_IP_ADDRESS = pi_hostname
+# Fill in with raspberryPi IP adress
+PI_IP_ADDRESS = "1"
+
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect(PI_IP_ADDRESS)
 
 def send_command_to_controller(vhf, uhf_400, uhf_468, s_band, request_telemetry):
     """
     Builds the JSON command and sends it to the Raspberry Pi.
     """
-    json_command = {
+    status = {
         "status": {
             "vhf": vhf,
             "uhf_400": uhf_400,
@@ -28,28 +21,14 @@ def send_command_to_controller(vhf, uhf_400, uhf_468, s_band, request_telemetry)
         },
         "request_telemetry": request_telemetry
     }
-
-    try:
-        response = requests.post(PI_IP_ADDRESS, json=json_command, timeout=3)
-        
-        if response.status_code == 200: #Success
-            print("Command successfully received by the Pi!")
-            return response.json() # Raspberry Pi response
-        else:
-            print(f"Error: Response code {response.status_code}")
-            return None
-
-    except requests.exceptions.RequestException as e:
-        print(f"Network Error: Could not find the Raspberry Pi. {e}")
-        return None
+    
+    socket.send_json(status)
+    
+    reply = socket.recv_json()
+    store_response(reply)
     
 def store_response(response):
     pass
 
-if __name__ == "__main__":
-    while True:
-        reply = send_command_to_controller(vhf="tx", uhf_400="rx", uhf_468="off", s_band="rx", request_telemetry=True)
-    
-        if reply:
-            store_response(reply)
+            
             
--- software/pi_command_handler.py
+++ software/pi_command_handler.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, jsonify
+import zmq
 import RPi.GPIO as GPIO
 from read_sensor import *
 
 GPIO.setmode(GPIO.BCM)
+
 # VHF ------------------------------------------
 pin_switch_vhf = 18
 pin_lna_vhf = 4
@@ -23,7 +24,9 @@ pin_lna_uhf_468 = 24
 pin_lna_s_band = 25
 #-----------------------------------------------
 
-app = Flask(__name__)
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+
 
 def toggle_vhf(state):
     if state == "rx":
@@ -63,44 +66,46 @@ def toggle_s_band(state):
     else:
         GPIO.output(pin_lna_s_band, GPIO.LOW)
 
-def read_telemetry():
-    return {}
+def get_telemetry():
+    return read_sensors()
 
-@app.route('/api/command', methods=['POST'])
 def interpret_command():
     """
-    Receives the JSON file from the lab Server, interprets the dictionary, 
-    and toggles the pins associated with the required hardware.
+    Receives the JSON file from the lab Server, interprets the dictionary
+    and toggles the GPIO pins associated with the required hardware.
     """
-    rcv_json = request.get_json()
-    
-    if not rcv_json or 'status' not in rcv_json:
-        return 1
-    
-    status = rcv_json.get('status', {})
-    
-    # Toggle the GPIO pins
-    if "vhf" in status:
-        toggle_vhf(status['vhf'])
+    with socket.bind("tcp://0.0.0.0:5555"):
+        try:
+            while True:
+                rcv_json = socket.recv_json()
+                
+                if not rcv_json:
+                    return 1
+                
+                status = rcv_json.get('status', {})
+                
+                # Toggle the GPIO pins
+                if "vhf" in status:
+                    toggle_vhf(status['vhf'])
+                    
+                if "uhf_400"in status:
+                    toggle_uhf_400(status['uhf_400'])
+                    
+                if "uhf_468" in status:
+                    toggle_uhf_468(status["uhf_468"])
+                    
+                if "s_band" in status:
+                    toggle_s_band(status["s_band"])
+                
+                response_data = {}
+
+                # Telemetry
+                if rcv_json.get('request_telemetry') == "true":
+                    response_data["telemetry"] = get_telemetry()
+                    
+                socket.send_json(response_data)
+        except KeyboardInterrupt:
+            pass
         
-    if "uhf_400"in status:
-        toggle_uhf_400(status['uhf_400'])
-        
-    if "uhf_468" in status:
-        toggle_uhf_468(status["uhf_468"])
-        
-    if "s_band" in status:
-        toggle_s_band(status["s_band"])
-    
-    response_data = {}
-
-    # Telemetry
-    if rcv_json.get('request_telemetry'):
-        response_data["telemetry"] = read_sensors()
-
-
-    return jsonify(response_data), 200
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    interpret_command()
